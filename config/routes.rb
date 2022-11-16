f77ed050c7f70d482123ea280d5fff174427c724
Alexandre Ferreira <alex.ferreira9915@gmail.com>
2022-11-16 12:55:03 +0000

    Aula 28  exercicio 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'restaurants#index'
+
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+
+  get 'restaurants/:id', to: 'restaurants#show', as: "restaurant"
+
+
+  get "restaurants/:id/reviews/new", to: "reviews#new", as: "new_restaurant_review"
+  post "restaurants/:id/reviews/", to: "reviews#create", as: 'restaurant_reviews'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+bk = {name: "BK", address: "No do Fojo", category: "french", phone_number: "123456789"}
+mc = {name: "MC", address: "Vila do Conde", category: "chinese"}
+pizza = {name: "Pizza", address: "Póvoa de Varzim", category: "italian", phone_number: "123456789"}
+kebab = {name: "Kebab", address: "Viana do Castelo", category: "japanese"}
+massa = {name: "Massa", address: "Porto", category: "belgian"}
+
+[bk, mc, pizza, kebab, massa].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
